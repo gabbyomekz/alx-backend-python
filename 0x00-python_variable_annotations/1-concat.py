bd32a9eefb34e96f0949dd5026c04d70a36929d2
Omeke Gabriel <106773800+gabbyomekz@users.noreply.github.com>
2023-05-01 14:37:52 +0100

    ad

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function concat two strings and concatenated it"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings"""
+    return str1 + str2
+  
