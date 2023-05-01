c4e7be5da1a9d82851a6a77b7a312c4f924c474d
Omeke Gabriel <106773800+gabbyomekz@users.noreply.github.com>
2023-05-01 15:23:30 +0100

    ad

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a single function that adds two integers together."""
+
+
+def add(a: float, b: float) -> float:
+    """ Add two integers together."""
+    return a + b
+  
