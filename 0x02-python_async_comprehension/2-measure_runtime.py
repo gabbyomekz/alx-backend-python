6323f3e00eb41d4fe1917b15cb9025853868633f
Omeke Gabriel <106773800+gabbyomekz@users.noreply.github.com>
2023-05-03 21:53:56 +0100

    asy

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Run time for four parallel comprehensions"""
+
+import asyncio
+from time import perf_counter
+from typing import List
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> List[float]:
+    """
+    Measures the time it takes to execute async_comprehension 4 times
+    in parallel using asyncio.gather().
+    Returns:
+        float: the total runtime in seconds.
+    """
+    starts = perf_counter()
+    # run async_comprehension four times in parallel using asyncio.gather
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    ends = perf_counter()
+    return ends - starts
+  
