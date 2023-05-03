19ea6d68db6722c55c940d01fe60e854472f7375
Omeke Gabriel <106773800+gabbyomekz@users.noreply.github.com>
2023-05-03 21:52:54 +0100

    asy

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Async generator"""
+
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """
+    Coroutine function that asynchronously yields 10 random numbers
+    between 0 and 10 after each second delay.
+    Yields:
+        float: random float number between 0 and 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)  # asynchronously wait for 1 second
+        yield random.uniform(0, 10)  # generate random float num btw 0 & 10
